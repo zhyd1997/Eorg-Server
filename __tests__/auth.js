7ee3b8dcbff1d09b7e03cf21a11bb50ee71b757f
007tom <zhyd007@gmail.com>
2021-09-14 21:06:29 +0800

    added login and logout tests.

--- __tests__/auth.js
+++ __tests__/auth.js
@@ -12,6 +12,8 @@ describe("auth", () => {
     db.disconnect(done);
   });
 
+  let tokenNoBearerPrefix;
+
   describe("register", () => {
     it("registers successfully", (done) => {
       const mockedUser = {
@@ -92,6 +94,79 @@ describe("auth", () => {
           const { error } = res.body;
 
           expect(error).toBe("User not found");
+
+          done();
+        })
+        .catch((err) => {
+          console.log(err);
+          done(err);
+        });
+    });
+
+    it("throws an error when password is not matched", (done) => {
+      const mockedUser = {
+        username: "test",
+        email: "test@test.com",
+        password: "test1234",
+      };
+
+      request(app)
+        .post(`${AUTH_URI}/login`)
+        .send(mockedUser)
+        .expect(401)
+        .then((res) => {
+          const { error } = res.body;
+          expect(error).toBe("Password is incorrect");
+          done();
+        })
+        .catch((err) => {
+          console.log(err);
+          done(err);
+        });
+    });
+
+    it("login sucessfully", (done) => {
+      const mockedUser = {
+        username: "test",
+        email: "test@test.com",
+        password: "test123",
+      };
+
+      request(app)
+        .post(`${AUTH_URI}/login`)
+        .send(mockedUser)
+        .expect(200)
+        .expect("set-cookie", /^token=/)
+        .then((res) => {
+          const { success, token, username } = res.body;
+
+          tokenNoBearerPrefix = token;
+
+          expect(success).toBe(true);
+          expect(token).toBeTruthy();
+          expect(username).toBe("test");
+
+          done();
+        })
+        .catch((err) => {
+          console.log(err);
+          done(err);
+        });
+    });
+  });
+
+  describe("logout", () => {
+    it("logout successfully", (done) => {
+      request(app)
+        .get(`${AUTH_URI}/logout`)
+        .auth(tokenNoBearerPrefix, { type: "bearer" })
+        .expect(200)
+        .expect("set-cookie", /^token=none/)
+        .then((res) => {
+          const { success, data } = res.body;
+
+          expect(success).toBe(true);
+          expect(data).toStrictEqual({});
 
           done();
         })
